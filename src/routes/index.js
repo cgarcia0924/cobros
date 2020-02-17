8322edbe8902714c674cc87f0d866381e10c8d35
cgarcia0924 <cgarcia0924@hotmail.com>
2020-02-16 22:27:49 -0500

    index

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { isLoggedIn } = require('../lib/auth');
 
 router.get('/', isLoggedIn, (req, res) => {
-    res.render('index/index');
+    res.render('index');
 });
 
 
