e5f559ace5099e4d21a81f0ec0169c64f0e0f87e
cgarcia0924 <cgarcia0924@hotmail.com>
2020-02-19 13:40:30 -0500

    cambio vista empleados

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -5,21 +5,21 @@ const pool = require('../database');
 
 const { isLoggedIn } = require('../lib/auth');
 
-router.get('/employees', async(req, res) => {
+router.get('/employees', isLoggedIn, async(req, res) => {
     // const employees = await pool.query('SELECT * FROM employees');
     //const links = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
     //res.render('employees/list', { employees });
     res.render('employees/list');
 });
 
-router.get('/employees/add', async(req, res) => {
+router.get('/employees/add', isLoggedIn, async(req, res) => {
     // const employees = await pool.query('SELECT * FROM employees');
     //const links = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
     //res.render('employees/list', { employees });
     res.render('employees/add');
 });
 
-router.get('/employees/edit', async(req, res) => {
+router.get('/employees/edit', isLoggedIn, async(req, res) => {
     // const employees = await pool.query('SELECT * FROM employees');
     //const links = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
     //res.render('employees/list', { employees });
--- src/routes/office.js
+++ src/routes/office.js
@@ -4,23 +4,25 @@ const router = express.Router();
 const pool = require('../database');
 
 const { isLoggedIn } = require('../lib/auth');
-router.get('/office', async(req, res) => {
-    // const office = await pool.query('SELECT * FROM office');
-    //const links = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
-    //res.render('office/list', { office });
-    res.render('office/list');
+
+router.get('/office', isLoggedIn, async(req, res) => {
+    const office = await pool.query('SELECT * FROM office');
+    console.log(office);
+    //const office = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
+    res.render('office/list', { office });
+
 });
 
-router.get('/office/add', async(req, res) => {
+router.get('/office/add', isLoggedIn, async(req, res) => {
     // const office = await pool.query('SELECT * FROM office');
-    //const links = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
+    //const office = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
     //res.render('office/list', { office });
     res.render('office/add');
 });
 
-router.get('/office/edit', async(req, res) => {
+router.get('/office/edit', isLoggedIn, async(req, res) => {
     // const office = await pool.query('SELECT * FROM office');
-    //const links = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
+    //const office = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
     //res.render('office/list', { office });
     res.render('office/edit');
 });
