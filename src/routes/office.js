4adf952e5a352c28c96c468ebc0ebbe388766b45
jose.corredor <jose.corredor@linqueate.com>
2020-02-20 17:56:49 -0500

    Ajustes Administrar oficina

--- src/routes/office.js
+++ src/routes/office.js
@@ -6,10 +6,11 @@ const pool = require('../database');
 const { isLoggedIn } = require('../lib/auth');
 
 router.get('/office', isLoggedIn, async(req, res) => {
-    const office = await pool.query('SELECT * FROM office');
-    console.log(office);
+    const office = await pool.query('SELECT * FROM office t1 INNER JOIN municipios t2 ON t1.city_id = t2.id;');
+    //console.log(office);
     //const office = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
     res.render('office/list', { office });
+    //res.render('office/add', { office });
 
 });
 
@@ -18,15 +19,15 @@ router.get('/office/delete/:id', async(req, res) => {
     //console.log(req.params.id);
     //res.send('Deleted');
     await pool.query('DELETE FROM office WHERE ID = ?', [id]);
-    req.flash('success', 'Link Removed Successfully');
+    req.flash('success', 'El registrado ha sido eliminado exitosamente');
     res.redirect('/office');
 });
 
 router.get('/office/add', isLoggedIn, async(req, res) => {
-    // const office = await pool.query('SELECT * FROM office');
+    const office = await pool.query('SELECT * FROM municipios');
     //const office = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
-    //res.render('office/list', { office });
-    res.render('office/add');
+    res.render('office/add', { office });
+    //res.render('office/add');
 });
 
 router.get('/office/edit', isLoggedIn, async(req, res) => {
