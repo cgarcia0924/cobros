496e79b72a05f54a0041f35a308904ee3bfce968
jose.corredor <jose.corredor@linqueate.com>
2020-02-21 22:58:39 -0500

    Ajustes administrar oficinas // Jose

--- src/routes/office.js
+++ src/routes/office.js
@@ -6,7 +6,7 @@ const pool = require('../database');
 const { isLoggedIn } = require('../lib/auth');
 //lista
 router.get('/office', isLoggedIn, async(req, res) => {
-    const office = await pool.query('SELECT * FROM office t1 INNER JOIN municipios t2 ON t1.city_id = t2.id;');
+    const office = await pool.query('SELECT b.id AS id, b.number_cel AS celular, b.names AS nombres, b.address AS direccion, b.city_id AS idcity, b.responsable AS responsable, b.customer_id AS idcliente, b.active AS active, a.id AS idmun, a.nombre AS nombremun FROM office AS b INNER JOIN municipios AS a ON b.id = a.id ORDER BY b.names ASC;');
     //console.log(office);
     //const office = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
     res.render('office/list', { office });
@@ -29,6 +29,8 @@ router.get('/office/add', isLoggedIn, async(req, res) => {
     res.render('office/add', { office });
     //res.render('office/add');
 });
+
+
 router.post('/office/add', async(req, res, done) => {
     const { number_cel, names, address, city_id, responsable, active = 'S' } = req.body;
     const newOffice = {
@@ -58,10 +60,32 @@ router.post('/office/add', async(req, res, done) => {
     }
 });
 //editar
-router.get('/office/edit', isLoggedIn, async(req, res) => {
-    // const office = await pool.query('SELECT * FROM office');
-    //const office = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
-    //res.render('office/list', { office });
-    res.render('office/edit');
+router.get('/edit/:id', async(req, res) => {
+    const { id } = req.params;
+    const users = await pool.query('SELECT b.id AS id, b.number_cel AS celular, b.names AS nombres, b.address AS direccion, b.city_id AS idcity, b.responsable AS responsable, b.customer_id AS idcliente, b.active AS active, a.id AS idmun, a.nombre AS nombremun FROM office AS b INNER JOIN municipios AS a ON b.id = a.id WHERE b.id = ?', [id]);
+    const office = await pool.query('SELECT * FROM municipios');
+    res.render('office/edit', { user: users[0], office });
 });
+
+
+
+
+router.post('/edit/:id', async(req, res) => {
+    const { id } = req.params;
+    const { number_cel, names, address, city_id, responsable, active = 'S' } = req.body;
+    const newuser = {
+        number_cel,
+        names,
+        address,
+        city_id,
+        responsable,
+        customer_id: req.user.customers_id,
+        active
+    };
+    console.log(newuser);
+    await pool.query('UPDATE office SET ? WHERE id = ?', [newuser, id]);
+    req.flash('success', 'Oficina actualizada correctamente');
+    res.redirect('/office');
+});
+
 module.exports = router;
